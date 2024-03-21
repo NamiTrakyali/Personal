8942a4dd85cad8a19fb4df4722fa1f6f26e2f1a1
NamiTrakyali <154099029+NamiTrakyali@users.noreply.github.com>
2024-03-21 18:46:23 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -2,10 +2,10 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://media.discordapp.net/attachments/737348411568685066/954502826791428136/codeshare_logo.png",
-      name: "Code Share",
-      description: "Discord türkiyenin en büyük discord bot komutları arşivi ve en büyük discord botu uptime, hostlayıcı websitesi. Code Share veritabanında discord botlarına özgü yüzlerce discord bot komutu bulunmaktadır ve kendi discord botunuzu yaratmanıza yardımcı olur.",
-      link: "https://codeshare.me",
+      image: "https://cdn.discordapp.com/attachments/1218985386538434702/1220397413655646359/rH7DQrC.png?ex=660ecaa9&is=65fc55a9&hm=ef5c4d9d2024fc8a4c87282e5b9d1041ffa5f2052a31f0c18c661ca9a09f410b&",
+      name: "Destek Bileti",
+      description: "Ticket/Bilet sistemidir sunucunuzda bilet açıp yetkililerin o bilete bakmasını sağlar. Kategori,Ban,Unban gibi komutları bulunmaktadır.",
+      link: "https://glitch.com/edit/#!/rebel-royal-dryer",
     },
     {
       id: 2,
