cbc2481d9176488a062b7e13400d543db731db29
NamiTrakyali <154099029+NamiTrakyali@users.noreply.github.com>
2024-03-22 01:48:46 +0300

    Update repos.jsx

--- pages/api/repos.jsx
+++ pages/api/repos.jsx
@@ -1,7 +1,7 @@
 const fetch = require('isomorphic-unfetch')
 
-let key1 = 'ghp_VzcC7gx0GXzOVP1';
-let key2 = '7kIiTmNtBqj2sOC0S0zxD';
+let key1 = 'ghp_PegBswG1pSXlCqd';
+let key2 = 'IPXeJM5NjphA8Tx0NCDMH';
 
 export default async (req, res) => {
     let _ = await (await fetch('https://github.com/NamiTrakyali?tab=repositories', {
