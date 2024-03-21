01cf2d24318c6382bb8b29454f037b4a416a214c
NamiTrakyali <154099029+NamiTrakyali@users.noreply.github.com>
2024-03-21 19:17:13 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,11 +16,11 @@ export default function Home() {
               <div className="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full">
                 <div className="flex items-center">
                 <p className="flex items-center text-white text-4xl font-semibold">
-                  Umut Bayraktar
+                  Swingler
                 </p>
                   <Tippy content={`Online`} animation="shift-away" arrow={false}>
                     <span className={`ml-2 text-online px-2 py-1 font-normal rounded-md text-sm`}>
-                          <i className={`fa fa-circle text-online mr-2`} />Online
+                          <i className={`fa fa-circle text-online mr-2`} />İdle
                     </span>
                   </Tippy>
             
@@ -30,9 +30,9 @@ I am a professional editor and software developer. I have been professionally in
                 </p>
               </div>
               <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
-                <img alt="umutbayraktar" src={`https://media.discordapp.net/attachments/737348411568685066/996841172372824184/YT_AVATAR_NET.jpg`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
+                <img alt="swingler" src={`https://cdn.discordapp.com/attachments/1218985386538434702/1220399520865583166/oOx7rZ5.png?ex=660ecc9f&is=65fc579f&hm=f30659ecb97e5944eb756da867b524770c2ab0eda927c381d2a5f5d8f5423c3e&`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
                 <div className={`bg-[#040404] rounded-full px-[4px] py-[1px] flex items-center absolute bottom-0 right-4`}>
-                  <Tippy content="Online" animation="shift-away" arrow={false}>
+                  <Tippy content="İdle" animation="shift-away" arrow={false}>
                     <i className={`fad fa-circle text-2xl text-online`} />
                   </Tippy>
                 </div>
